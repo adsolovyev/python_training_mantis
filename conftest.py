92561438ed46814432d000527cfcee0ad26613ee
anreysolovyev <86119184+anreysolovyev@users.noreply.github.com>
2021-07-07 19:26:05 +0300

    tests - login, add and create project

---
+++ conftest.py
@@ -0,0 +1,58 @@
+import json
+import pytest
+import os.path
+from fixture.application import Application
+from fixture.db import DbFixture
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture
+def app(request, config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = config["web"]
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
+    return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request, config):
+    db_config = config["db"]
+    db_fixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+
+    def fin():
+        db_fixture.destroy()
+    request.addfinalizer(fin)
+    return db_fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
